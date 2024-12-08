e60cb467c73972cde4ac46356d00d5dac87b9291
Samuel Afolabi <1samuelafolabi@gmail.com>
2024-12-08 22:44:52 +0100

    added url for sitemap

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,74 +1,68 @@
-import getFormattedDate from "@/lib/getFormattedDate"
-import { getPostsMeta, getPostByName } from "@/lib/posts"
-import { notFound } from "next/navigation"
-import Link from "next/link"
-import 'highlight.js/styles/github-dark.css'
+import getFormattedDate from "@/lib/getFormattedDate";
+import { getPostsMeta, getPostByName } from "@/lib/posts";
+import { notFound } from "next/navigation";
+import Link from "next/link";
+import "highlight.js/styles/github-dark.css";
 
-export const revalidate = 86400
+export const revalidate = 86400;
 
 type Props = {
-    params: {
-        postId: string
-    }
-}
+  params: {
+    postId: string;
+  };
+};
 
 export async function generateStaticParams() {
-    const posts = await getPostsMeta() //deduped!
+  const posts = await getPostsMeta(); //deduped!
 
-    if (!posts) return []
+  if (!posts) return [];
 
-    return posts.map((post) => ({
-        postId: post.id
-    }))
+  return posts.map((post) => ({
+    postId: post.id,
+  }));
 }
 
 export async function generateMetadata({ params: { postId } }: Props) {
+  const post = await getPostByName(`${postId}.mdx`); //deduped!
 
-    const post = await getPostByName(`${postId}.mdx`) //deduped!
-
-    if (!post) {
-        return {
-            title: 'Post Not Found'
-        }
-    }
-
+  if (!post) {
     return {
-        title: post.meta.title,
-    }
+      title: "Post Not Found",
+    };
+  }
+
+  return {
+    title: post.meta.title,
+  };
 }
 
 export default async function Post({ params: { postId } }: Props) {
+  const post = await getPostByName(`${postId}.mdx`); //deduped!
 
-    const post = await getPostByName(`${postId}.mdx`) //deduped!
+  if (!post) notFound();
 
-    if (!post) notFound()
+  const { meta, content } = post;
 
-    const { meta, content } = post
+  const pubDate = getFormattedDate(meta.date);
 
-    const pubDate = getFormattedDate(meta.date)
+  const tags = meta.tags.map((tag, i) => (
+    <Link key={i} href={`/tags/${tag}`}>
+      {tag}
+    </Link>
+  ));
 
-    const tags = meta.tags.map((tag, i) => (
-        <Link key={i} href={`/tags/${tag}`}>{tag}</Link>
-    ))
-
-    return (
-        <>
-            <h2 className="text-3xl mt-4 mb-0">{meta.title}</h2>
-            <p className="mt-0 text-sm">
-                {pubDate}
-            </p>
-            <article>
-                {content}
-            </article>
-            <section>
-                <h3>Related:</h3>
-                <div className="flex flex-row gap-4">
-                    {tags}
-                </div>
-            </section>
-            <p className="mb-10">
-                <Link href="/">← Back to home</Link>
-            </p>
-        </>
-    )
+  return (
+    <>
+      <h2 className="text-3xl mt-4 mb-0">{meta.title}</h2>
+      <p className="mt-0 text-sm">{pubDate}</p>
+      <article>{content}</article>
+      <section>
+        <h3>Related:</h3>
+        <div className="flex flex-row gap-4">{tags}</div>
+      </section>
+      <p className="mb-10">
+        <Link href="/">← Back to home</Link>
+      </p>
+    </>
+  );
 }
